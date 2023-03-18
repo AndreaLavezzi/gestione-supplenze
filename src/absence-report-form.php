cdee2dcbccef20fd4fd9e9f08a22a9b60707f330
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2023-03-18 12:18:34 +0100

    Finita pagina html di report dell'assenza con i campi necessari, rifinita responsività della pagina

--- src/absence-report-form.php
+++ src/absence-report-form.php
@@ -7,9 +7,9 @@
         <h4 class="mb-3">Segnala la tua assenza</h4>
         <form class="needs-validation" novalidate="">
             <div class="row g-3">
-                <div class="col-md-4">
-                    <label for="state" class="form-label">Nome docente</label>
-                    <select class="form-select" id="state" required="" disabled>
+                <div class="col-md-5 me-5">
+                    <label for="name" class="form-label">Nome docente</label>
+                    <select class="form-select" id="name" required="" disabled>
                         <option value="">Inserire il docente</option>
                         <option>Raspa Massimiliano</option>
                         <option>Dall'Ara Enrico Ermanno</option>
@@ -24,146 +24,81 @@
                     </div>
                 </div>
 
-                <div class="col-12">
-                    <label for="date" class="form-label">Data</label>
-                    <input type="text" class="form-control" id="date" placeholder="gg/mm/aaaa" required="">
-                </div>
-
-                <div class="col-12">
-                    <label for="username" class="form-label">Username</label>
-                    <div class="input-group has-validation">
-                        <span class="input-group-text">@</span>
-                        <input type="text" class="form-control" id="username" placeholder="Username" required="">
-                        <div class="invalid-feedback">
-                            Your username is required.
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-12">
-                    <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
-                    <input type="email" class="form-control" id="email" placeholder="you@example.com">
+                <div class="col-md-5">
+                    <label for="absence-type" class="form-label">Tipo di assenza</label>
+                    <select class="form-select" id="absence-type" required="">
+                        <option value="">Inserire il tipo di assenza</option>
+                        <option>Singolo giorno</option>
+                        <option>Gruppo di giorni</option>
+                        <option>Singola ora</option>
+                        <option>Gruppo di ore</option>
+                    </select>
                     <div class="invalid-feedback">
-                        Please enter a valid email address for shipping updates.
+                        Please provide a valid name
                     </div>
                 </div>
 
-                <div class="col-12">
-                    <label for="address" class="form-label">Address</label>
-                    <input type="text" class="form-control" id="address" placeholder="1234 Main St" required="">
-                    <div class="invalid-feedback">
-                        Please enter your shipping address.
-                    </div>
-                </div>
-
-                <div class="col-12">
-                    <label for="address2" class="form-label">Address 2 <span
-                            class="text-muted">(Optional)</span></label>
-                    <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
+                <div class="col-md-5 me-5">
+                    <label for="date" class="form-label">Da</label>
+                    <input type="date" class="form-control" id="date" placeholder="gg/mm/aaaa" required="">
                 </div>
 
                 <div class="col-md-5">
-                    <label for="country" class="form-label">Country</label>
-                    <select class="form-select" id="country" required="">
-                        <option value="">Choose...</option>
-                        <option>United States</option>
-                    </select>
-                    <div class="invalid-feedback">
-                        Please select a valid country.
-                    </div>
+                    <label for="date" class="form-label">A</label>
+                    <input type="date" class="form-control" id="date" placeholder="gg/mm/aaaa" required="">
                 </div>
 
-                <div class="col-md-4">
-                    <label for="state" class="form-label">State</label>
-                    <select class="form-select" id="state" required="">
-                        <option value="">Choose...</option>
-                        <option>California</option>
-                    </select>
-                    <div class="invalid-feedback">
-                        Please provide a valid state.
-                    </div>
-                </div>
-
-                <div class="col-md-3">
-                    <label for="zip" class="form-label">Zip</label>
-                    <input type="text" class="form-control" id="zip" placeholder="" required="">
-                    <div class="invalid-feedback">
-                        Zip code required.
-                    </div>
-                </div>
-            </div>
-
+                
             <hr class="my-4">
-
+            <h4 class="mb-3">Ore di assenza</h4>
             <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="same-address">
-                <label class="form-check-label" for="same-address">Shipping address is the same as my billing
-                    address</label>
+                <input type="checkbox" class="form-check-input" id="hour-one">
+                <label class="form-check-label" for="same-address">1^ ora</label>
             </div>
 
             <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="save-info">
-                <label class="form-check-label" for="save-info">Save this information for next time</label>
+                <input type="checkbox" class="form-check-input" id="hour-two">
+                <label class="form-check-label" for="same-address">2^ ora</label>
+            </div>
+
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="hour-three">
+                <label class="form-check-label" for="same-address">3^ ora</label>
+            </div>
+
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="hour-four">
+                <label class="form-check-label" for="same-address">4^ ora</label>
+            </div>
+
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="hour-five">
+                <label class="form-check-label" for="same-address">5^ ora</label>
             </div>
 
             <hr class="my-4">
 
-            <h4 class="mb-3">Payment</h4>
-
-            <div class="my-3">
-                <div class="form-check">
-                    <input id="credit" name="paymentMethod" type="radio" class="form-check-input" checked=""
-                        required="">
-                    <label class="form-check-label" for="credit">Credit card</label>
-                </div>
-                <div class="form-check">
-                    <input id="debit" name="paymentMethod" type="radio" class="form-check-input" required="">
-                    <label class="form-check-label" for="debit">Debit card</label>
-                </div>
-                <div class="form-check">
-                    <input id="paypal" name="paymentMethod" type="radio" class="form-check-input" required="">
-                    <label class="form-check-label" for="paypal">PayPal</label>
-                </div>
+            <div class="col-md-5 me-5">
+                    <label for="name" class="form-label">Motivo</label>
+                    <select class="form-select" id="name" required="">
+                        <option value="">Inserire il motivo</option>
+                        <option>Salute</option>
+                        <option>Vacanza</option>
+                        <option>Motivi personali</option>
+                    </select>
+                    <div class="invalid-feedback">
+                        Please provide a valid name
+                    </div>
             </div>
-
-            <div class="row gy-3">
-                <div class="col-md-6">
-                    <label for="cc-name" class="form-label">Name on card</label>
-                    <input type="text" class="form-control" id="cc-name" placeholder="" required="">
-                    <small class="text-muted">Full name as displayed on card</small>
-                    <div class="invalid-feedback">
-                        Name on card is required
-                    </div>
+            
+            <div class="col-md-6 ">
+                    <label for="disease-protocol-number" class="form-label">Numero protocollo malattia</label>
+                    <input type="text" class="form-control" id="disease-protocol-number" placeholder="Inserire il numero di protocollo malattia" required="">
                 </div>
-
-                <div class="col-md-6">
-                    <label for="cc-number" class="form-label">Credit card number</label>
-                    <input type="text" class="form-control" id="cc-number" placeholder="" required="">
-                    <div class="invalid-feedback">
-                        Credit card number is required
-                    </div>
-                </div>
-
-                <div class="col-md-3">
-                    <label for="cc-expiration" class="form-label">Expiration</label>
-                    <input type="text" class="form-control" id="cc-expiration" placeholder="" required="">
-                    <div class="invalid-feedback">
-                        Expiration date required
-                    </div>
-                </div>
-
-                <div class="col-md-3">
-                    <label for="cc-cvv" class="form-label">CVV</label>
-                    <input type="text" class="form-control" id="cc-cvv" placeholder="" required="">
-                    <div class="invalid-feedback">
-                        Security code required
-                    </div>
-                </div>
-            </div>
 
             <hr class="my-4">
 
-            <button class="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
+            <button class="w-100 btn btn-primary btn-lg" type="submit">Invia</button>
         </form>
     </div>
 </main>

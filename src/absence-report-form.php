cf7e7bc4de20dbacc75ace718c2a6a8a85f2d814
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2023-03-20 16:05:10 +0100

    Migliorato il posizionamento dei campi del form

--- src/absence-report-form.php
+++ src/absence-report-form.php
@@ -3,102 +3,127 @@
 
 <main>
 
-    <div class="col-md-7 col-lg-8">
-        <h4 class="mb-3">Segnala la tua assenza</h4>
+    <div class="col-12">
+
         <form class="needs-validation" novalidate="">
             <div class="row g-3">
-                <div class="col-md-5 me-5">
-                    <label for="name" class="form-label">Nome docente</label>
-                    <select class="form-select" id="name" required="" disabled>
-                        <option value="">Inserire il docente</option>
-                        <option>Raspa Massimiliano</option>
-                        <option>Dall'Ara Enrico Ermanno</option>
-                        <option>Mazzullo Alessandro</option>
-                        <option>Visentin Alessandro</option>
-                        <option>Lamantea Cesare Nicola</option>
-                        <option>Manfrini Anita</option>
+                <h4 class="">Segnala la tua assenza</h4>
 
-                    </select>
-                    <div class="invalid-feedback">
-                        Please provide a valid name
+                <div class="row g-2">
+                    <div class="col-md-5">
+                        <label for="name" class="form-label ms-1">Nome docente</label>
+                        <select class="form-select" id="name" required="">
+                            <option value="">Inserire il docente</option>
+                            <option>Raspa Massimiliano</option>
+                            <option>Dall'Ara Enrico Ermanno</option>
+                            <option>Mazzullo Alessandro</option>
+                            <option>Visentin Alessandro</option>
+                            <option>Lamantea Cesare Nicola</option>
+                            <option>Manfrini Anita</option>
+
+                        </select>
+                        <div class="invalid-feedback">
+                            Please provide a valid name
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-md-5">
-                    <label for="absence-type" class="form-label">Tipo di assenza</label>
-                    <select class="form-select" id="absence-type" required="">
-                        <option value="">Inserire il tipo di assenza</option>
-                        <option>Singolo giorno</option>
-                        <option>Gruppo di giorni</option>
-                        <option>Singola ora</option>
-                        <option>Gruppo di ore</option>
-                    </select>
-                    <div class="invalid-feedback">
-                        Please provide a valid name
+
+                <div class="row g-2">
+                    <div class="col-md-5">
+                        <label for="absence-type" class="form-label ms-1">Tipo di assenza</label>
+                        <select class="form-select" id="absence-type" required="">
+                            <option value="0">Inserire il tipo di assenza</option>
+                            <option value="1">Singolo giorno</option>
+                            <option value="2">Gruppo di giorni</option>
+                            <option value="3">Singola ora</option>
+                            <option value="4">Gruppo di ore</option>
+                        </select>
+                        <div class="invalid-feedback">
+                            Please provide a valid name
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-md-5 me-5">
-                    <label for="date" class="form-label">Da</label>
-                    <input type="date" class="form-control" id="date" placeholder="gg/mm/aaaa" required="">
-                </div>
-
-                <div class="col-md-5">
-                    <label for="date" class="form-label">A</label>
-                    <input type="date" class="form-control" id="date" placeholder="gg/mm/aaaa" required="">
-                </div>
-
-                
-            <hr class="my-4">
-            <h4 class="mb-3">Ore di assenza</h4>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="hour-one">
-                <label class="form-check-label" for="same-address">1^ ora</label>
-            </div>
-
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="hour-two">
-                <label class="form-check-label" for="same-address">2^ ora</label>
-            </div>
-
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="hour-three">
-                <label class="form-check-label" for="same-address">3^ ora</label>
-            </div>
-
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="hour-four">
-                <label class="form-check-label" for="same-address">4^ ora</label>
-            </div>
-
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="hour-five">
-                <label class="form-check-label" for="same-address">5^ ora</label>
-            </div>
-
-            <hr class="my-4">
-
-            <div class="col-md-5 me-5">
-                    <label for="name" class="form-label">Motivo</label>
-                    <select class="form-select" id="name" required="">
-                        <option value="">Inserire il motivo</option>
-                        <option>Salute</option>
-                        <option>Vacanza</option>
-                        <option>Motivi personali</option>
-                    </select>
-                    <div class="invalid-feedback">
-                        Please provide a valid name
+                <div class="row g-2">
+                    <div class="col-md-5">
+                        <label for="date" class="form-label ms-1">Da</label>
+                        <input type="date" class="form-control" id="date-from" placeholder="gg/mm/aaaa" required="">
                     </div>
-            </div>
-            
-            <div class="col-md-6 ">
-                    <label for="disease-protocol-number" class="form-label">Numero protocollo malattia</label>
-                    <input type="text" class="form-control" id="disease-protocol-number" placeholder="Inserire il numero di protocollo malattia" required="">
+
+                    <div class="col-md-5">
+                        <label for="date" class="form-label ms-1">A</label>
+                        <input type="date" class="form-control" id="date-to" placeholder="gg/mm/aaaa" required="">
+                    </div>
                 </div>
 
-            <hr class="my-4">
+                <div id="hours">
+                    <label for="hours" class="form-label">Ore di assenza</label>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="hour-one">
+                        <label class="form-check-label" for="same-address">1^ ora</label>
+                    </div>
 
-            <button class="w-100 btn btn-primary btn-lg" type="submit">Invia</button>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="hour-two">
+                        <label class="form-check-label" for="same-address">2^ ora</label>
+                    </div>
+
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="hour-three">
+                        <label class="form-check-label" for="same-address">3^ ora</label>
+                    </div>
+
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="hour-four">
+                        <label class="form-check-label" for="same-address">4^ ora</label>
+                    </div>
+
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="hour-five">
+                        <label class="form-check-label" for="same-address">5^ ora</label>
+                    </div>
+                </div>
+
+                <div class="row g-2">
+                    <div class="col-md-5">
+                        <label for="name" class="form-label ms-1">Motivo</label>
+                        <select class="form-select" id="name" required="">
+                            <option value="">Inserire il motivo</option>
+                            <option>Salute</option>
+                            <option>Vacanza</option>
+                            <option>Motivi personali</option>
+                        </select>
+                        <div class="invalid-feedback">
+                            Please provide a valid name
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 ">
+                        <label for="disease-protocol-number" class="form-label ms-1">Numero protocollo malattia</label>
+                        <input type="text" class="form-control" id="disease-protocol-number"
+                            placeholder="Inserire il numero di protocollo malattia" required="">
+                    </div>
+                </div>
+
+
+
+                <hr class="my-4">
+
+                <div class="d-flex justify-content-center align-content-center" id="button">
+                    <div class="w-50 btn btn-primary btn-lg" id="send-button">Invia</div>
+                </div>
         </form>
     </div>
+
+    <script>
+        $("#send-button").click(function () {
+            var absence_type = $("#absence-type option:selected");
+            var name = $("#name option:selected");
+            var date_from = new Date($("#date-from").val());
+            var date_to = new Date($("#date-to").val());
+            var check = [];
+            console.log(absence_type.text(), name.text(), date_from, date_to.text(), check);
+        });
+    </script>
 </main>
--- src/index.php
+++ src/index.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link href="css/mobiscroll.jquery.min.css" rel="stylesheet" type="text/css">
     <title>Document</title>
 </head>
 <style>
@@ -17,12 +16,9 @@
 </style>
 
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
-        integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.js"
+        integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous">
+    </script>
 
 
     <div class="container">
